40ce23940fb0fe8b23cd7c5a342f6e6a53c317e4
shibamoto <silverbirder@gmail.com>
2020-06-11 07:40:44 +0900

    Format

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,5 +1,5 @@
 module "provider" {
-  source = "../../modules/providers"
+  source         = "../../modules/providers"
   primary_region = var.primary_region
-  project_id = var.project_id
+  project_id     = var.project_id
 }
